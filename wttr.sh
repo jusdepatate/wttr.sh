f4ff0e12d2a8a097fd196710ca18cb368114be57
~jusdepatate <yaume@ntymail.com>
2018-11-23 20:31:46 +0000

    Added argument support

--- wttr.sh
+++ wttr.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 # wttr.sh uses wttr.in's api
 # Jus de Patate (yaume@ntymail.com) - CC-BY-NC
-# Version 1.0
+# Version 1.1
 
-if [ "$(ping -c 1 wttr.in)" ]; then
-  if [ "$(which curl 2>/dev/null)" ]; then
-    CITY="$(curl -s ipinfo.io/city)"
-		echo "Weather of $CITY"
-		curl -s "wttr.in/$CITY?Q0"
-	elif [ "$(which wget 2>/dev/null)" ]; then
-		CITY="$(wget -qO- ipinfo.io)"
-		echo "Weather of $CITY"
-		wget -qO- "wttr.in/$CITY?Q0"
-	fi
-echo
+if [ "$1" != null ]; then
+        if [ "$(ping -c 1 wttr.in)" ]; then
+        if [ "$(which curl 2>/dev/null)" ]; then
+            CITY="$(curl -s ipinfo.io/city)"
+                        echo "Weather of $1"
+                        curl -s "wttr.in/$1?Q0"
+                elif [ "$(which wget 2>/dev/null)" ]; then
+                        CITY="$(wget -qO- ipinfo.io)"
+                        echo "Weather of $1"
+                        wget -qO- "wttr.in/$1?Q0"
+                fi
+        echo
+        else
+                echo "Can't connect to wttr.in"
+        fi
 else
-	echo "Can't connect to wttr.in"
+        if [ "$(ping -c 1 wttr.in)" ]; then
+        if [ "$(which curl 2>/dev/null)" ]; then
+          CITY="$(curl -s ipinfo.io/city)"
+                        echo "Weather of $CITY"
+                        curl -s "wttr.in/$CITY?Q0"
+                elif [ "$(which wget 2>/dev/null)" ]; then
+                        CITY="$(wget -qO- ipinfo.io)"
+                        echo "Weather of $CITY"
+                        wget -qO- "wttr.in/$CITY?Q0"
+                fi
+        echo
+        else
+                echo "Can't connect to wttr.in"
+        fi
 fi
